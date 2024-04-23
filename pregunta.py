54ade3894d83424e251ff9830fd3c828275c5a4f
Caaminp <160285915+Caaminp@users.noreply.github.com>
2024-04-22 22:21:24 -0500

    intento 2

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,29 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
+import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    with open('clusters_report.txt', 'r') as file:
+        data = []
+        for line in file:
+            columns= line.split()  
+            if len(columns) >=1: 
+                    try:
+                        cluster = int(columns[0]) 
+                        cantidad_de_palabras_clave = int(columns[1]) 
+                        porcentaje_de_palabras_clave = columns[2] 
+                        principales_palabras_clave= ' '.join(columns[4:]) 
+                        data.append([cluster, cantidad_de_palabras_clave, porcentaje_de_palabras_clave, principales_palabras_clave])
+                    except:
+                        if len(data) > 0:
+                            principales_palabras_clave= ' '.join(columns)
+                            data[len(data)-1][3] += ' ' + principales_palabras_clave 
 
+    df = pd.DataFrame(data, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(',','.').astype(float)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('.','')
+    df.columns= df.columns.str.replace(' ', '_').str.lower()
     return df
+print(ingest_data())       
